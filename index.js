4b17269d13a5b9fb2f0ab055a2d202e892c1916e
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-04-15 13:46:00 +0300

    add comander and remove yargs

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const contacts = require("./contacts");
-const argv = require("yargs").argv;
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
