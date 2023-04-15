b9679dfe3291ea317f3f08dab9df49578f61c273
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-04-15 13:30:49 +0300

    completed

---
+++ index.js
@@ -0,0 +1,27 @@
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
