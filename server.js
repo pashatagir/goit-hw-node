7c353ece5538d35d7d0d6c4f5bc78447581b31af
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-04-26 10:52:25 +0300

    fix

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => {
+    app.listen(PORT);
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
