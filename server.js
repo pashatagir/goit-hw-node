62911d9b3a875045e47fa83b0ba0db16c8b157d1
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-04-26 10:04:48 +0300

    added process.env

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://pashtetik:123pashtetik987@cluster1.0yggomk.mongodb.net/contacts_reader?retryWrites=true&w=majority";
-
 mongoose.set("strictQuery", true);
+
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
